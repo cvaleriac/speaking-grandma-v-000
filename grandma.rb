5ca60cdbf53065497ca32b39c29ebf2c38fd3995
valeria campilongo <cvaleriac@hotmail.it>
2018-04-07 23:29:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
-def speak_to_grandma (phrase = "I love you grandma!")
-  if phrase.upcase
+def speak_to_grandma (phrase)
+  if phrase == "I love you grandma!".upcase
     return  "I LOVE YOU TOO PUMPKIN!"
   elsif phrase
     return "HUH?! SPEAK UP, SONNY!"
-  else return "NO, NOT SINCE 1938!"
+  else phrase.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
 
