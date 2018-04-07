ae5cba6c0831306b38640a514913397f4b8a839d
valeria campilongo <cvaleriac@hotmail.it>
2018-04-07 23:09:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,12 +5,13 @@ def speak_to_grandma
     return "HUH?! SPEAK UP, SONNY!"
   else return "NO, NOT SINCE 1938!"
   end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
